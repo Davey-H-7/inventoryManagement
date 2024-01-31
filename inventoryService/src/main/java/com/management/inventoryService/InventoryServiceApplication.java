0ee43c250a0ae6e0538247aa2fdea4be10854e7b
Davey-H-7 <138510681+Davey-H-7@users.noreply.github.com>
2024-01-31 15:27:03 +0000

    first commit

---
+++ inventoryService/src/main/java/com/management/inventoryService/InventoryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.management.inventoryService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryServiceApplication.class, args);
+	}
+
+}
