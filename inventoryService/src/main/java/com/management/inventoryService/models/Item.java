d6a73e8a51def7e9fc5471b91f5b319b7f66a773
Davey-H-7 <138510681+Davey-H-7@users.noreply.github.com>
2024-02-07 19:26:42 +0000

    added item model

---
+++ inventoryService/src/main/java/com/management/inventoryService/models/Item.java
@@ -0,0 +1,49 @@
+package com.management.inventoryService.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "items")
+public class Item {
+
+    private String name;
+
+    private String model;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    public Item(String name, String model, Long id) {
+        this.name = name;
+        this.model = model;
+        this.id = id;
+    }
+
+    public Item() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+}
