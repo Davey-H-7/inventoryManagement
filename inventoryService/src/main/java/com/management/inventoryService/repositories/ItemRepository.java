cd36b2c51bdf6d71096a9e3f3d8183009a6f9593
Davey-H-7 <138510681+Davey-H-7@users.noreply.github.com>
2024-02-07 22:51:46 +0000

    added item repository

---
+++ inventoryService/src/main/java/com/management/inventoryService/repositories/ItemRepository.java
@@ -0,0 +1,9 @@
+package com.management.inventoryService.repositories;
+
+import com.management.inventoryService.models.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ItemRepository extends JpaRepository<Item, Long> {
+}
