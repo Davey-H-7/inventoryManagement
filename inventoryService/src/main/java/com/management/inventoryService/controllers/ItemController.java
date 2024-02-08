8d96fa4095345daf38019026404bfeee49e64786
Davey-H-7 <138510681+Davey-H-7@users.noreply.github.com>
2024-02-08 17:08:47 +0000

    post route added

--- inventoryService/src/main/java/com/management/inventoryService/controllers/ItemController.java
+++ inventoryService/src/main/java/com/management/inventoryService/controllers/ItemController.java
@@ -5,9 +5,7 @@ import com.management.inventoryService.repositories.ItemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,5 +23,11 @@ public class ItemController {
     @GetMapping(value ="/items/{id}")
     public ResponseEntity getItem(@PathVariable Long id){
         return new ResponseEntity<>(itemRepository.findById(id), HttpStatus.OK);
+    }
+
+    @PostMapping(value ="/items")
+    public ResponseEntity<Item> postItem(@RequestBody Item item){
+        itemRepository.save(item);
+        return new ResponseEntity<>(item, HttpStatus.OK);
     }
 }
