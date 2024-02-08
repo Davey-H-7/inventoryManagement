8151b1d8573e47271b88c5e6fc263a4c6962fcef
Davey-H-7 <138510681+Davey-H-7@users.noreply.github.com>
2024-02-08 17:01:18 +0000

    get route added for speicic item by id

--- inventoryService/src/main/java/com/management/inventoryService/controllers/ItemController.java
+++ inventoryService/src/main/java/com/management/inventoryService/controllers/ItemController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -19,5 +20,10 @@ public class ItemController {
     @GetMapping(value ="/items")
     public ResponseEntity<List<Item>> getAllItems(){
         return new ResponseEntity<>(itemRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(value ="/items/{id}")
+    public ResponseEntity getItem(@PathVariable Long id){
+        return new ResponseEntity<>(itemRepository.findById(id), HttpStatus.OK);
     }
 }
