1253d5e336da40ff32797a5e8f290354213d7f04
Davey-H-7 <138510681+Davey-H-7@users.noreply.github.com>
2024-02-19 21:36:32 +0000

    added orders to dataloader

--- inventoryService/src/main/java/com/management/inventoryService/components/DataLoader.java
+++ inventoryService/src/main/java/com/management/inventoryService/components/DataLoader.java
@@ -1,34 +1,48 @@
 package com.management.inventoryService.components;
 
 import com.management.inventoryService.models.Item;
+import com.management.inventoryService.models.Order;
 import com.management.inventoryService.repositories.ItemRepository;
+import com.management.inventoryService.repositories.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
-//@Profile("!test")
-//@Component
+
+@Profile("!test")
+@Component
 public class DataLoader implements ApplicationRunner {
 
     @Autowired
     ItemRepository itemRepository;
 
+    @Autowired
+    OrderRepository orderRepository;
+
     public DataLoader() {
     }
 
     public void run(ApplicationArguments args){
-        itemRepository.save(new Item("Pipe", 100, "Steel pipe for industrial use",null));
-        itemRepository.save(new Item("Wheel", 50, "Wheel component for automobiles", null));
-        itemRepository.save(new Item("Fitting", 200, "Pipe fitting for plumbing applications", null));
-        itemRepository.save(new Item("Gearbox", 20, "Transmission component for machinery", null));
-        itemRepository.save(new Item("Valve", 150, "Control valve for fluid regulation", null));
-        itemRepository.save(new Item("Bearing", 80, "Mechanical bearing for rotating machinery", null));
-        itemRepository.save(new Item("Conduit", 120, "Electrical conduit for wire protection", null));
-        itemRepository.save(new Item("Gasket", 300, "Sealing gasket for leak-proof connections", null));
-        itemRepository.save(new Item("Bracket", 70, "Support bracket for structural reinforcement", null));
-        itemRepository.save(new Item("Hose", 180, "Flexible hose for fluid transfer", null));
+        Order order1 = new Order("Building Boys");
+        orderRepository.save(order1);
+        Order order2 = new Order("Car Cannibals");
+        orderRepository.save(order2);
+        Order order3 = new Order("Factory Friends");
+        orderRepository.save(order3);
+
+
+        itemRepository.save(new Item("Pipe", 100, "Steel pipe for industrial use",order1));
+        itemRepository.save(new Item("Wheel", 50, "Wheel component for automobiles", order2));
+        itemRepository.save(new Item("Fitting", 200, "Pipe fitting for plumbing applications", order1));
+        itemRepository.save(new Item("Gearbox", 20, "Transmission component for machinery", order2));
+        itemRepository.save(new Item("Valve", 150, "Control valve for fluid regulation", order3));
+        itemRepository.save(new Item("Bearing", 80, "Mechanical bearing for rotating machinery", order3));
+        itemRepository.save(new Item("Conduit", 120, "Electrical conduit for wire protection", order1));
+        itemRepository.save(new Item("Gasket", 300, "Sealing gasket for leak-proof connections", order2));
+        itemRepository.save(new Item("Bracket", 70, "Support bracket for structural reinforcement", order1));
+        itemRepository.save(new Item("Hose", 180, "Flexible hose for fluid transfer", order2));
 
 
     }
