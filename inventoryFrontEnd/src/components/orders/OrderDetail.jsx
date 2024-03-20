b33a1f543bf6112d5940852508dcc0a79abff410
Davey-H-7 <138510681+Davey-H-7@users.noreply.github.com>
2024-03-20 23:33:34 +0000

    styling updates on order detail

--- inventoryFrontEnd/src/components/orders/OrderDetail.jsx
+++ inventoryFrontEnd/src/components/orders/OrderDetail.jsx
@@ -9,14 +9,18 @@ const OrderDetail = ({order, handleDelete}) => {
 
     return(
         <div className="detail">
-            <h1>Order Number: {order.id}</h1>
-            <h2>Client: {order.client}</h2>
-            <h3>Due on: {order.dueDate}</h3>
-            <h3> Items in order:</h3>
-            <ul>
+            <div className="detailInfo">
+                <h1>Order Number: {order.id}</h1>
+                <h2>Client: {order.client}</h2>
+                <h3>Due on: {order.dueDate}</h3>
+            </div>
+            <ul className="detailItemList">
+                <h3> Items in order:</h3>
                 {detailItems}
             </ul>
-            <button onClick = {() => handleDelete(order)}>Delete order</button>
+            <form>
+                <button onClick = {() => handleDelete(order)}>Delete order</button>
+            </form>
         </div>
         
     )
