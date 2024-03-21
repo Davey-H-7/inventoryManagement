1cb41bbc56a55804c27bf24202456a17e50d9c7d
Davey-H-7 <138510681+Davey-H-7@users.noreply.github.com>
2024-03-21 14:16:51 +0000

    added welcome message and buttons to landing page

--- inventoryFrontEnd/src/containers/LandingContainer.jsx
+++ inventoryFrontEnd/src/containers/LandingContainer.jsx
@@ -1,6 +1,18 @@
 const LandingContainer = () => {
-    return (  
-        <h1>Welcome</h1>
+
+
+    return (
+        <div className="landingContainer">
+            <h1>Home</h1>
+            <h2>Welcome to Fabric8</h2>
+            <h3> Then you can add items and assign them to an order!</h3>
+            <h3> If this is your first time using the software then please begin by adding your first order.</h3>
+            <h4> Click below to see your orders or items</h4>
+            
+            <button onClick={() => window.location ="/orders"}>View My Orders</button>
+            <button onClick={() => window.location ="/items"}>View My Items</button>
+            
+        </div>
     );
 }
  
