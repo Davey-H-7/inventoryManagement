2a34fd01cb70b364c4ce6c972114e2f60b7aa125
Davey-H-7 <138510681+Davey-H-7@users.noreply.github.com>
2024-03-28 16:53:23 +0000

    added to part detail

--- inventoryFrontEnd/src/components/parts/PartDetail.jsx
+++ inventoryFrontEnd/src/components/parts/PartDetail.jsx
@@ -1,5 +1,33 @@
 const PartDetail = ({part, items, handleDelete}) => {
-    return ( <h1>{part.model}</h1> );
+    let totalQuantity = 0
+  
+    const partItems = items.filter((partItem) => partItem.part.id == part.id)
+    
+    const itemNodes= partItems.map((item) => {
+      return <li key ={item.id}>Order No:{item.order.id} for {item.order.client} {item.quantity}</li>
+    })
+
+    const calculateTotal = () => {
+      partItems.forEach(element => {
+        totalQuantity += element.quantity;
+      });
+    }
+
+    calculateTotal();
+
+    return (
+    <div className="partDetail">
+        <h1>{part.model}</h1>
+        <h2>{part.description}</h2>
+        <h3> Total quantity across orders: {totalQuantity}</h3>
+        <h3>Included in orders:</h3>
+        <ul>
+            {itemNodes}
+        </ul>
+        <button onClick = {()=> window.location="/parts"}>Return to Parts</button>
+        <button onClick={() => handleDelete(part)}> Delete Part </button>
+    </div>
+    )
 }
  
 export default PartDetail;
