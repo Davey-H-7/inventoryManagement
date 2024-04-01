0acbe778f61aef41db43bcf46baeda91d847d11e
Davey-H-7 <138510681+Davey-H-7@users.noreply.github.com>
2024-04-01 12:10:35 +0100

    added part form to add new parts

---
+++ inventoryFrontEnd/src/components/parts/PartForm.jsx
@@ -0,0 +1,7 @@
+const PartForm = ({handlePost}) => {
+    return ( 
+        <h1>This is the part form</h1>
+     )
+}
+ 
+export default PartForm;
--- inventoryFrontEnd/src/containers/PartContainer.jsx
+++ inventoryFrontEnd/src/containers/PartContainer.jsx
@@ -1,9 +1,20 @@
 import { Routes, Route, useParams } from "react-router-dom";
 import PartList from "../components/parts/PartList";
 import PartDetail from "../components/parts/PartDetail";
+import PartForm from "../components/parts/PartForm";
 import '../styling/parts/PartContainer.css'
 
 const PartContainer = ({parts, items, getById}) => {
+
+  const handlePost = (order) =>{
+    console.log("handlePost triggered");
+    fetch("/api/orders", {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(order)
+    })
+    .then(() => window.location ='/orders')
+}
 
     const handleDelete = (part) => {
         fetch("/api/parts/" + part.id, {
@@ -22,7 +33,7 @@ const PartContainer = ({parts, items, getById}) => {
         <div className="partsContainer">
         <Routes>
             <Route path="/" element = {<PartList parts ={parts}/>}/>
-            {/* <Route path ="/new" element = {<PartForm handlePost ={handlePost} /> }/> */}
+            <Route path ="/new" element = {<PartForm handlePost ={handlePost} /> }/>
             <Route path ="/:id" element = {<PartDetailWrapper/>}/>
         </Routes>
 
