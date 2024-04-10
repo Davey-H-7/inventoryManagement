32e32a6af7052e2d93ff20804884b85267609934
Davey-H-7 <138510681+Davey-H-7@users.noreply.github.com>
2024-04-10 15:02:01 +0100

    updated title on landing page

--- inventoryFrontEnd/src/containers/LandingContainer.jsx
+++ inventoryFrontEnd/src/containers/LandingContainer.jsx
@@ -7,7 +7,7 @@ const LandingContainer = () => {
         <div className="landingContainer">
         
             <h1>Home</h1>
-            <h2>Welcome to Fabric8</h2>
+            <h2>Welcome to Maker Tracker</h2>
             <h3> If this is your first time using the software then please begin by adding your first order.</h3>
             <h3> Then you can add items and assign them to an order!</h3>
 
